80d562cfe8ced3314507ae354aeeb84811eec12f
Alex Kubrick <alexandra.kamenchyk@mail.ru>
2024-05-23 00:14:02 +0700

    Chapter 25. Challenge 2

--- app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -3,9 +3,12 @@ package com.bignerdranch.android.sunset
 import android.animation.AnimatorSet
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AccelerateInterpolator
+import android.view.animation.AnimationUtils
 import androidx.core.content.ContextCompat
 import com.bignerdranch.android.sunset.databinding.ActivityMainBinding
 
@@ -44,10 +47,32 @@ class MainActivity : AppCompatActivity() {
         val sunYStart = binding.sun.top.toFloat()
         val sunYEnd = binding.sky.height.toFloat()
 
+        val haloYStart = binding.halo.top.toFloat()
+        val haloYEnd = binding.sky.height.toFloat()
+
         val heightAnimator = ObjectAnimator
             .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
             .setDuration(3000)
         heightAnimator.interpolator = AccelerateInterpolator()
+
+        val heightHaloAnimator = ObjectAnimator
+            .ofFloat(binding.halo, "y", haloYStart, haloYEnd)
+            .setDuration(3000)
+        heightHaloAnimator.interpolator = AccelerateInterpolator()
+
+        val haloSpinAnimator = ObjectAnimator
+            .ofFloat(binding.halo, "rotation", 0f, 360f)
+            .setDuration(3000)
+
+        val sunPulseAnimator = ValueAnimator.ofFloat(1f, 1.1f, 1f)
+        sunPulseAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            binding.sun.scaleX = value
+        }
+        sunPulseAnimator.repeatMode = ValueAnimator.REVERSE
+        sunPulseAnimator.repeatCount = ObjectAnimator.INFINITE
+        sunPulseAnimator.duration = 500L
+        sunPulseAnimator.start()
 
         val sunsetSkyAnimator = ObjectAnimator
             .ofInt(binding.sky, "backgroundColor", blueSkyColor, sunsetSkyColor)
@@ -61,10 +86,11 @@ class MainActivity : AppCompatActivity() {
 
         val animatorSet = AnimatorSet()
         animatorSet.play(heightAnimator)
+            .with(heightHaloAnimator)
+            .with(haloSpinAnimator)
             .with(sunsetSkyAnimator)
             .before(nightSkyAnimator)
         animatorSet.start()
-
     }
 
     // chapter 25. challenge 1.
@@ -72,10 +98,18 @@ class MainActivity : AppCompatActivity() {
         val sunYStart = binding.sun.top.toFloat()
         val sunYEnd = binding.sky.bottom.toFloat()
 
+        val haloYStart = binding.halo.top.toFloat()
+        val haloYEnd = binding.sky.height.toFloat()
+
         val heightAnimator = ObjectAnimator
             .ofFloat(binding.sun, "y", sunYEnd, sunYStart)
             .setDuration(3000)
         heightAnimator.interpolator = AccelerateInterpolator()
+
+        val heightHaloAnimator = ObjectAnimator
+            .ofFloat(binding.halo, "y", haloYEnd, haloYStart)
+            .setDuration(3000)
+        heightHaloAnimator.interpolator = AccelerateInterpolator()
 
         val sunsetSkyAnimator = ObjectAnimator
             .ofInt(binding.sky, "backgroundColor", nightSkyColor, sunsetSkyColor)
@@ -89,6 +123,7 @@ class MainActivity : AppCompatActivity() {
 
         val animatorSet = AnimatorSet()
         animatorSet.play(heightAnimator)
+            .with(heightHaloAnimator)
             .with(sunsetSkyAnimator)
             .before(normalSkyAnimator)
         animatorSet.start()
